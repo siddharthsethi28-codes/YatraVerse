f39bb2244d3e37693baeb1f28ab6db3c9e65e721
siddharthsethi28-codes <sethisiddharth97@gmail.com>
2026-08-04 10:32:18 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,17 +1,17 @@
 import os
 import pymysql
-pymysql.install_as_MySQLdb()
+import pymysql.cursors
 
-from flask_mysqldb import MySQL
-
-mysql = MySQL()
+def get_db_connection():
+    return pymysql.connect(
+        host=os.getenv('DB_HOST', 'localhost'),
+        port=int(os.getenv('DB_PORT', 3306)),
+        user=os.getenv('DB_USER', 'root'),
+        password=os.getenv('DB_PASSWORD', ''),
+        database=os.getenv('DB_NAME', 'wanderhub_db'),
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
 def init_db(app):
-    app.config['MYSQL_HOST']     = os.getenv('DB_HOST', 'localhost')
-    app.config['MYSQL_PORT']     = int(os.getenv('DB_PORT', 3306))
-    app.config['MYSQL_USER']     = os.getenv('DB_USER', 'root')
-    app.config['MYSQL_PASSWORD'] = os.getenv('DB_PASSWORD', '')
-    app.config['MYSQL_DB']       = os.getenv('DB_NAME', 'wanderhub_db')
-    app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-    mysql.init_app(app)
-    return mysql
+    # Flask app initialization if needed
+    pass
